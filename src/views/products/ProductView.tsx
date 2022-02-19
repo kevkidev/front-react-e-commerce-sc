2da4bf177d212d93c8c79ab1f4b69cb1dd35b6f5
kevkidev <>
2022-02-19 18:01:34 +0100

    subcommit: edit product

--- src/views/products/ProductView.tsx
+++ src/views/products/ProductView.tsx
@@ -1,16 +1,27 @@
 // import "./AccountPage.scss";
+import { useLayoutEffect, useState } from "react";
 import { Table } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { Auth } from "../../components/AuthContainer";
-import { RoutesPath } from "../../routes";
 import { RestService } from "../../services/RestService";
-import CreateProductModal from "./CreateProductModal";
+import ProductBuilderModal from "./ProductBuilderModal";
 
 export default function ProductView() {
+  // const [selectedProduct, selectProduct] = useState<Model.Product>();
+  const [value, setValue] = useState("init");
+
+  useLayoutEffect(() => {
+    console.log(value);
+  }, [value]);
+
   return (
     <Auth.Container>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
       <h1>My Products</h1>
-      <CreateProductModal />
+      <ProductBuilderModal triggerTitle="New Product" />
       <hr />
       <Table striped bordered hover>
         <thead>
@@ -21,17 +32,40 @@ export default function ProductView() {
           </tr>
         </thead>
         <tbody>
-          {RestService.products.map((p, i) => (
-            <tr key={i + p.uid}>
-              <td>{i}</td>
-              <td>
-                <Link to={RoutesPath.PRODUCT}>{p.name}</Link>
-              </td>
-              <td>{p.quantity}</td>
-            </tr>
-          ))}
+          {RestService.products.map((p, i) => {
+            const row = (
+              <>
+                <td>{i}</td>
+                <td>
+                  {p.name}
+                  {/* <Link to={RoutesPath.PRODUCT + "/" + p.uid}>{p.name}</Link> */}
+                  {/* <a to={RoutesPath.PRODUCT + "/" + p.uid}>{p.name}</Link> */}
+                  {/* <ProductBuilderModal
+                    triggerTitle={p.name}
+                    productId={p.uid}
+                    triggerContent
+                  /> */}
+                </td>
+                <td>{p.quantity}</td>
+              </>
+            );
+            return (
+              <ProductBuilderModal
+                triggerAsContent
+                triggerTitle={row}
+                productId={p.uid}
+                key={i + p.uid}
+              />
+            );
+          })}
         </tbody>
       </Table>
+      {/* <ProductBuilderModal
+        triggerAsContent
+        triggerTitle={row}
+        productId={p.uid}
+        key={i + p.uid}
+      /> */}
     </Auth.Container>
   );
 }
