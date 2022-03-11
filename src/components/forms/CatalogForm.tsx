8b4db6a23d4e89459ee9c8bdee46e2c029bc1a61
kevkidev <7610777+kevkidev@users.noreply.github.com>
2022-03-11 21:25:39 +0100

    Factorise  CatalogForm and ProductForm: - clean code Product and Catalog files - unlink ProductFormModal - regenerate dep. graph

--- src/components/forms/CatalogForm.tsx
+++ src/components/forms/CatalogForm.tsx
@@ -3,9 +3,8 @@ import _ from "lodash";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { DTO } from "types/dto";
-import { CatalogStatus, FormAction } from "types/types";
-import { v4 as uuidv4 } from "uuid";
-import { object, string } from "yup";
+import { FormAction } from "types/types";
+import { availableStatus, defaultValue, schema } from "./catalogFormConfig";
 
 interface Props {
   formId: string;
@@ -15,37 +14,17 @@ interface Props {
   onSave: (value: DTO.Catalog) => void;
 }
 
-const emptyCatalog: DTO.Catalog = {
-  uid: uuidv4(),
-  imageUrl: "",
-  ownerUid: "",
-  productsUids: [],
-  title: "",
-  status: "alive",
-};
-
-const schema = object({
-  title: string()
-    .matches(/^[\w\W]{3,200}$/, "3 to 200 characters")
-    .required(),
-}).required();
-
-type FormInputs = {
-  title: string;
-  status: string;
-};
-
-export default function Catalogform(props: Props) {
+export default function CatalogForm(props: Props) {
   const { formId, resetTrigger, catalog, onSave, action } = props;
   const [value, setValue] = useState<DTO.Catalog>(
-    catalog ? catalog : emptyCatalog
+    catalog ? catalog : defaultValue
   );
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
-  } = useForm<FormInputs>({
+  } = useForm<DTO.Catalog>({
     resolver: yupResolver(schema),
   });
 
@@ -53,19 +32,13 @@ export default function Catalogform(props: Props) {
     reset({ title: value.title, status: value.status });
   }, [resetTrigger, reset]);
 
-  const onSubmit = (data: FormInputs) => {
+  const onSubmit = (data: DTO.Catalog) => {
     console.log(data);
     const newCatalog = _.cloneDeep(value);
     newCatalog.title = data.title;
     setValue(newCatalog);
     onSave(newCatalog);
   };
-
-  const availableStatus: { label: string; value: CatalogStatus }[] = [
-    { label: "Alive", value: "alive" },
-    { label: "Archived", value: "archived" },
-    { label: "Disabled", value: "disabled" },
-  ];
 
   const renderStatus = (
     <>
