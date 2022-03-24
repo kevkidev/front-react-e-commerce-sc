cced701a843dfc06fa71f7b780ccb71ca2613c9a
kevkidev <7610777+kevkidev@users.noreply.github.com>
2022-03-24 18:04:36 +0100

    Cleancode + refacto Catalog: - create and use generic create from Firebase

--- src/services/cloud/firebase/CatalogFirebaseService.ts
+++ src/services/cloud/firebase/CatalogFirebaseService.ts
@@ -7,9 +7,7 @@ import {
   query,
   QueryConstraint,
   ref,
-  set,
   startAfter,
-  update,
 } from "firebase/database";
 import { Firebase } from "services/Firebase";
 import { DTO } from "types/dto";
@@ -17,21 +15,17 @@ import { ListResultLimits } from "types/types";
 
 export namespace CatalogFirebaseService {
   export function create(value: DTO.Catalog) {
-    const db = getDatabase();
-    const PATH = `${value.ownerUid}/${Firebase.DB_NODE_CATALOGS}/${value.uid}`;
-    db &&
-      set(ref(db, PATH), value).then(() =>
-        console.log(`Database: create catalog: ${value.uid}`)
-      );
+    const path = `${value.ownerUid}/${Firebase.DB_NODE_CATALOGS}/${value.uid}`;
+    Firebase.create(value, path, () => {
+      console.log(`Database: create catalog: ${value.uid}`);
+    });
   }
 
   export function modify(value: DTO.Catalog) {
-    const db = getDatabase();
-    const PATH = `${value.ownerUid}/${Firebase.DB_NODE_CATALOGS}/${value.uid}`;
-    db &&
-      update(ref(db, PATH), value).then(() =>
-        console.log(`Database: update catalog: ${value.uid}`)
-      );
+    const path = `${value.ownerUid}/${Firebase.DB_NODE_CATALOGS}/${value.uid}`;
+    Firebase.modify(value, path, () => {
+      console.log(`Database: update catalog: ${value.uid}`);
+    });
   }
 
   interface IReadAll {
@@ -40,36 +34,28 @@ export namespace CatalogFirebaseService {
     listResultLimits?: ListResultLimits;
     onExists: (value: DTO.Catalog[], limits: ListResultLimits) => void;
   }
-
-  // @todo: transform as genic method
   export function readAll(args: IReadAll) {
     const { ownerUid, maxItems, onExists, listResultLimits } = args;
-
     const db = getDatabase();
-    const PATH = `${ownerUid}/catalogs/`;
+    const PATH = `${ownerUid}/${Firebase.DB_NODE_CATALOGS}/`;
     const constraints: QueryConstraint[] = [
       orderByKey(),
       limitToFirst(maxItems),
     ];
-
     if (listResultLimits) {
       const { firstKey, lastKey } = listResultLimits;
       if (firstKey) constraints.push(endBefore(firstKey));
       if (lastKey) constraints.push(startAfter(lastKey));
     }
 
-    const accountRef = query(ref(db, PATH), ...constraints);
-
-    onValue(accountRef, (snapshot: any) => {
+    const catalogQuery = query(ref(db, PATH), ...constraints);
+    onValue(catalogQuery, (snapshot: any) => {
       if (!snapshot.val()) return;
 
       const keys = Object.keys(snapshot.val());
-      const firstKey = keys[0];
-      const lastKey = keys[keys.length - 1];
-
       const limits = {
-        firstKey,
-        lastKey,
+        firstKey: keys[0],
+        lastKey: keys[keys.length - 1],
       };
       onExists(Object.values(snapshot.val()), limits);
     });
